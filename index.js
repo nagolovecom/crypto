4f90989fbb5065eff0bf04c7bdaf619d5c236e3f
nagolovecom <102730736+nagolovecom@users.noreply.github.com>
2022-03-31 13:05:10 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+"Settings": {
+        "$type": "System.Collections.Generic.Dictionary`2[[System.String, mscorlib],[System.Object, mscorlib]], mscorlib",
+        "lastUsedUnitId": {
+              "1": "units-3368",
+              "$type": "System.Collections.Generic.Dictionary`2[[System.Int32, mscorlib],[System.String, mscorlib]], mscorlib"
+          }
+}
